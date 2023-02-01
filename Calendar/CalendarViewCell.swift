a33afc8565122285f93cf5a8129b2dea846d48d0
Rustam Manafli <rustemmanafov1@gmail.com>
2023-02-01 11:30:55 +0400

    updated

---
+++ Calendar/CalendarViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarViewCell.swift
+//  Calendar
+//
+//  Created by Rustem Manafov on 01.02.23.
+//
+
+import UIKit
+
+class CalendarViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    
+}
